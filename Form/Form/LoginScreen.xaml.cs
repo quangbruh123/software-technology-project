b413b02ac6e2782d09e7b6000986a9025d17ba92
Truongducthien <80139154+Truongducthien@users.noreply.github.com>
2022-03-10 14:04:12 +0700

    Add files via upload
    Test wpf

---
+++ Form/Form/LoginScreen.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Form
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection("@");
+        }
+    }
+}
