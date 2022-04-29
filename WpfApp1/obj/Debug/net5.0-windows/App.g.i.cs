1a05686c6098bb5286fa2ca9e436dc74d5e74032
Truongducthien <80139154+Truongducthien@users.noreply.github.com>
2022-04-29 21:12:15 +0700

    ui

--- WpfApp1/obj/Debug/net5.0-windows/App.g.i.cs
+++ WpfApp1/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace WpfApp1 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/App;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/App;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
