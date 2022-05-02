dfa196c3d1ae95e2a8a4565c95a1828633f95499
Truongducthien <80139154+Truongducthien@users.noreply.github.com>
2022-05-02 16:17:43 +0700

    Lam lai

---
+++ ĐỒÁN/ĐỒÁN/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace ĐỒÁN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tiepnhanbaotrixebutton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.editrulebutton = new System.Windows.Forms.Button();
+            this.functionbutton = new System.Windows.Forms.Button();
+            this.searchbutton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lapbaocaothangbutton = new System.Windows.Forms.Button();
+            this.lapphieuthutienbutton = new System.Windows.Forms.Button();
+            this.lapphieusuachuabutton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.baocaotonbutton = new System.Windows.Forms.Button();
+            this.baocaodoanhsobutton = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panelsetting = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lapphieusuachuaUserControl1 = new ĐỒÁN.usercontrol.LapphieusuachuaUserControl();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tiepnhanbaotrixebutton
+            // 
+            this.tiepnhanbaotrixebutton.Location = new System.Drawing.Point(0, 17);
+            this.tiepnhanbaotrixebutton.Name = "tiepnhanbaotrixebutton";
+            this.tiepnhanbaotrixebutton.Size = new System.Drawing.Size(135, 32);
+            this.tiepnhanbaotrixebutton.TabIndex = 0;
+            this.tiepnhanbaotrixebutton.Text = "1.Tiếp nhận bảo trì xe";
+            this.tiepnhanbaotrixebutton.UseVisualStyleBackColor = true;
+            this.tiepnhanbaotrixebutton.Click += new System.EventHandler(this.tiepnhanbaotrixebutton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(63)))), ((int)(((byte)(84)))));
+            this.panel1.Controls.Add(this.editrulebutton);
+            this.panel1.Controls.Add(this.searchbutton);
+            this.panel1.Controls.Add(this.functionbutton);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(105, 612);
+            this.panel1.TabIndex = 0;
+            // 
+            // editrulebutton
+            // 
+            this.editrulebutton.Location = new System.Drawing.Point(-1, 167);
+            this.editrulebutton.Name = "editrulebutton";
+            this.editrulebutton.Size = new System.Drawing.Size(93, 34);
+            this.editrulebutton.TabIndex = 0;
+            this.editrulebutton.Text = "Chỉnh sửa quy định";
+            this.editrulebutton.UseVisualStyleBackColor = true;
+            // 
+            // functionbutton
+            // 
+            this.functionbutton.Location = new System.Drawing.Point(-1, 47);
+            this.functionbutton.Name = "functionbutton";
+            this.functionbutton.Size = new System.Drawing.Size(93, 34);
+            this.functionbutton.TabIndex = 0;
+            this.functionbutton.Text = "Chức năng";
+            this.functionbutton.UseVisualStyleBackColor = true;
+            this.functionbutton.Click += new System.EventHandler(this.functionbutton_Click);
+            // 
+            // searchbutton
+            // 
+            this.searchbutton.Location = new System.Drawing.Point(-1, 99);
+            this.searchbutton.Name = "searchbutton";
+            this.searchbutton.Size = new System.Drawing.Size(93, 34);
+            this.searchbutton.TabIndex = 0;
+            this.searchbutton.Text = "Tra cứu";
+            this.searchbutton.UseVisualStyleBackColor = true;
+            this.searchbutton.Click += new System.EventHandler(this.searchbutton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(63)))), ((int)(((byte)(84)))));
+            this.panel2.Controls.Add(this.lapbaocaothangbutton);
+            this.panel2.Controls.Add(this.lapphieuthutienbutton);
+            this.panel2.Controls.Add(this.lapphieusuachuabutton);
+            this.panel2.Controls.Add(this.tiepnhanbaotrixebutton);
+            this.panel2.Location = new System.Drawing.Point(103, 47);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(136, 565);
+            this.panel2.TabIndex = 1;
+            // 
+            // lapbaocaothangbutton
+            // 
+            this.lapbaocaothangbutton.Location = new System.Drawing.Point(0, 162);
+            this.lapbaocaothangbutton.Name = "lapbaocaothangbutton";
+            this.lapbaocaothangbutton.Size = new System.Drawing.Size(135, 32);
+            this.lapbaocaothangbutton.TabIndex = 0;
+            this.lapbaocaothangbutton.Text = "4.Lập báo cáo tháng";
+            this.lapbaocaothangbutton.UseVisualStyleBackColor = true;
+            this.lapbaocaothangbutton.Click += new System.EventHandler(this.lapbaocaothangbutton_Click);
+            // 
+            // lapphieuthutienbutton
+            // 
+            this.lapphieuthutienbutton.Location = new System.Drawing.Point(0, 114);
+            this.lapphieuthutienbutton.Name = "lapphieuthutienbutton";
+            this.lapphieuthutienbutton.Size = new System.Drawing.Size(135, 32);
+            this.lapphieuthutienbutton.TabIndex = 0;
+            this.lapphieuthutienbutton.Text = "3.Lập phiếu thu tiền";
+            this.lapphieuthutienbutton.UseVisualStyleBackColor = true;
+            this.lapphieuthutienbutton.Click += new System.EventHandler(this.lapphieuthutienbutton_Click);
+            // 
+            // lapphieusuachuabutton
+            // 
+            this.lapphieusuachuabutton.Location = new System.Drawing.Point(0, 64);
+            this.lapphieusuachuabutton.Name = "lapphieusuachuabutton";
+            this.lapphieusuachuabutton.Size = new System.Drawing.Size(135, 32);
+            this.lapphieusuachuabutton.TabIndex = 0;
+            this.lapphieusuachuabutton.Text = "2.Lập phiếu sửa chữa";
+            this.lapphieusuachuabutton.UseVisualStyleBackColor = true;
+            this.lapphieusuachuabutton.Click += new System.EventHandler(this.lapphieusuachuabutton_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(63)))), ((int)(((byte)(84)))));
+            this.panel3.Controls.Add(this.baocaotonbutton);
+            this.panel3.Controls.Add(this.baocaodoanhsobutton);
+            this.panel3.Location = new System.Drawing.Point(104, 50);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(138, 562);
+            this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // baocaotonbutton
+            // 
+            this.baocaotonbutton.Location = new System.Drawing.Point(3, 90);
+            this.baocaotonbutton.Name = "baocaotonbutton";
+            this.baocaotonbutton.Size = new System.Drawing.Size(132, 35);
+            this.baocaotonbutton.TabIndex = 0;
+            this.baocaotonbutton.Text = "Báo cáo tồn";
+            this.baocaotonbutton.UseVisualStyleBackColor = true;
+            this.baocaotonbutton.Click += new System.EventHandler(this.baocaotonbutton_Click);
+            // 
+            // baocaodoanhsobutton
+            // 
+            this.baocaodoanhsobutton.Location = new System.Drawing.Point(3, 42);
+            this.baocaodoanhsobutton.Name = "baocaodoanhsobutton";
+            this.baocaodoanhsobutton.Size = new System.Drawing.Size(132, 35);
+            this.baocaodoanhsobutton.TabIndex = 0;
+            this.baocaodoanhsobutton.Text = "Báo cáo doanh số";
+            this.baocaodoanhsobutton.UseVisualStyleBackColor = true;
+            this.baocaodoanhsobutton.Click += new System.EventHandler(this.baocaodoanhsobutton_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Location = new System.Drawing.Point(105, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(723, 25);
+            this.panel4.TabIndex = 4;
+            // 
+            // panelsetting
+            // 
+            this.panelsetting.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelsetting.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panelsetting.Location = new System.Drawing.Point(105, 25);
+            this.panelsetting.Name = "panelsetting";
+            this.panelsetting.Size = new System.Drawing.Size(723, 25);
+            this.panelsetting.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(691, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(659, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.Location = new System.Drawing.Point(627, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(32, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // lapphieusuachuaUserControl1
+            // 
+            this.lapphieusuachuaUserControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lapphieusuachuaUserControl1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lapphieusuachuaUserControl1.Location = new System.Drawing.Point(0, 0);
+            this.lapphieusuachuaUserControl1.Name = "lapphieusuachuaUserControl1";
+            this.lapphieusuachuaUserControl1.Size = new System.Drawing.Size(828, 612);
+            this.lapphieusuachuaUserControl1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(828, 612);
+            this.Controls.Add(this.panelsetting);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lapphieusuachuaUserControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button editrulebutton;
+        private System.Windows.Forms.Button functionbutton;
+        private System.Windows.Forms.Button searchbutton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button lapbaocaothangbutton;
+        private System.Windows.Forms.Button lapphieuthutienbutton;
+        private System.Windows.Forms.Button lapphieusuachuabutton;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button baocaotonbutton;
+        private System.Windows.Forms.Button baocaodoanhsobutton;
+        private System.Windows.Forms.Button tiepnhanbaotrixebutton;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panelsetting;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private usercontrol.LapphieusuachuaUserControl lapphieusuachuaUserControl1;
+    }
+}
+
