fa9f1fbbcc82bc6616821ef40891cc7339d15443
Nguyễn Duy Hào <duyhao301202@gmail.com>
2022-05-05 21:57:00 +0700

    data handling class

---
+++ GarageManager/GarageManager/Classes/DataProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageManager.Classes
+{
+    public class DataProvider
+    {
+        private DataProvider instance;
+        public DataProvider Instance
+        {
+            get { return instance; }
+            set { instance = value; }
+        }
+    }
+}
