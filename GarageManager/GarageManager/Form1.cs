bc0c0b3cd98003b617000a283b6c4cbbe7a274cd
Nguyễn Duy Hào <duyhao301202@gmail.com>
2022-05-05 21:45:43 +0700

    project name changed

---
+++ GarageManager/GarageManager/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GarageManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Visible =true;
+            mainpanel.Visible = true;
+            lapphieuthutienUserControl.Visible = false;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            mainpanel.Visible = false;
+            lapphieuthutienUserControl.Visible = true;
+        }
+    }
+}
