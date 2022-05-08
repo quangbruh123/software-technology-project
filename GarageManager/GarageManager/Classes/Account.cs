5ba1a3e622f110d48c362b9332eef3d1c85a9c90
Nguyễn Duy Hào <duyhao301202@gmail.com>
2022-05-08 14:02:54 +0700

    password sha256 decode

--- GarageManager/GarageManager/Classes/Account.cs
+++ GarageManager/GarageManager/Classes/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,8 +19,7 @@ namespace GarageManager.Classes
         /// </returns>
         public bool AddStaffAccount(string username, string password)
         {
-            var userCount = DataProvider.Instance.DB.TAIKHOANs.Where(x => x.TenTaiKhoan == username && x.MatKhau == password).Count();
-            if (userCount == 0)
+            if (!DataProvider.Instance.DB.TAIKHOANs.Any(x => x.TenTaiKhoan == username && x.MatKhau == password))
             {
                 Model.TAIKHOAN newaccount = new Model.TAIKHOAN
                 {
@@ -44,8 +44,7 @@ namespace GarageManager.Classes
         /// </returns>
         public bool AddAdminAccount(string username, string password)
         {
-            var userCount = DataProvider.Instance.DB.TAIKHOANs.Where(x => x.TenTaiKhoan == username && x.MatKhau == password).Count();
-            if (userCount == 0)
+            if (!DataProvider.Instance.DB.TAIKHOANs.Any(x => x.TenTaiKhoan == username && x.MatKhau == password))
             {
                 Model.TAIKHOAN newaccount = new Model.TAIKHOAN
                 {
@@ -69,6 +68,51 @@ namespace GarageManager.Classes
             Model.TAIKHOAN unwantedAccount = DataProvider.Instance.DB.TAIKHOANs.Where(x => x.TenTaiKhoan == username).First();
             DataProvider.Instance.DB.TAIKHOANs.Remove(unwantedAccount);
             DataProvider.Instance.DB.SaveChanges();
+        }
+
+        /// <summary>
+        /// User log in check, set isLoggedIn to true if the user's account existed, false if it doesn't
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        private void LogIn(string username, string password)
+        {
+            SHA256 sha256hash = SHA256.Create();
+            string passwordhash = GetHash(sha256hash, password);
+            if(Classes.DataProvider.Instance.DB.TAIKHOANs.Any(x => x.TenTaiKhoan == username && VerifyHash(sha256hash, x.MatKhau, passwordhash)))
+                MainForm.isLoggedIn = true;
+        }
+
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        private static bool VerifyHash(HashAlgorithm hashAlgorithm, string input, string hash)
+        {
+            // Hash the input.
+            var hashOfInput = GetHash(hashAlgorithm, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
         }
     }
 }
---
+++ GarageManager/GarageManager/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using GarageManager.usercontrol;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GarageManager
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Log in state
+        /// </summary>
+        public static bool isLoggedIn = false;
+        //Nút nhảy usercontrol
+        private LapphieuthutienUserControl uc1;
+        private tracuuUserControl1 uc2;
+
+        public MainForm()
+        {
+            InitializeComponent();           
+        }  
+        //Nút exit zoom mini
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            mainpanel.Visible = true;
+            uc1 = new LapphieuthutienUserControl();
+            Controls.Add(uc1);
+            uc2 = new tracuuUserControl1();
+            Controls.Add(uc2);
+            uc1.Visible = false;
+            uc2.Visible = false;
+        }
+        private void tracuuxebutton_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            mainpanel.Visible = false;
+            uc1.Visible = false;
+            uc2.Visible = true;
+        }
+        private void lapphieuthutienbutton_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            mainpanel.Visible = false;
+            uc1.Visible = true;
+            uc2.Visible = false;
+        }        
+    }
+}
