5c3a93f07edfc7ed070e76696abc74e2262d914f
vsinh <20520737@ms.uit.edu.vn>
2022-06-08 12:37:42 +0700

    Kết nối chức năng báo cáo doanh số

--- Source code/GarageManager/GarageManager/usercontrol/Baocaodoanhso.Designer.cs
+++ Source code/GarageManager/GarageManager/usercontrol/Baocaodoanhso.Designer.cs
@@ -47,18 +47,20 @@
             // textBoxThangBaoCao
             // 
             this.textBoxThangBaoCao.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxThangBaoCao.Location = new System.Drawing.Point(339, 108);
+            this.textBoxThangBaoCao.Location = new System.Drawing.Point(452, 133);
+            this.textBoxThangBaoCao.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxThangBaoCao.Name = "textBoxThangBaoCao";
-            this.textBoxThangBaoCao.Size = new System.Drawing.Size(149, 26);
+            this.textBoxThangBaoCao.Size = new System.Drawing.Size(197, 30);
             this.textBoxThangBaoCao.TabIndex = 19;
             // 
             // labelThangBaoCao
             // 
             this.labelThangBaoCao.AutoSize = true;
             this.labelThangBaoCao.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelThangBaoCao.Location = new System.Drawing.Point(224, 111);
+            this.labelThangBaoCao.Location = new System.Drawing.Point(299, 137);
+            this.labelThangBaoCao.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelThangBaoCao.Name = "labelThangBaoCao";
-            this.labelThangBaoCao.Size = new System.Drawing.Size(53, 19);
+            this.labelThangBaoCao.Size = new System.Drawing.Size(69, 22);
             this.labelThangBaoCao.TabIndex = 17;
             this.labelThangBaoCao.Text = "Tháng :";
             // 
@@ -66,10 +68,10 @@
             // 
             this.btnBaoCaoDoanhSoMoi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnBaoCaoDoanhSoMoi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBaoCaoDoanhSoMoi.Location = new System.Drawing.Point(928, 102);
-            this.btnBaoCaoDoanhSoMoi.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBaoCaoDoanhSoMoi.Location = new System.Drawing.Point(1237, 126);
+            this.btnBaoCaoDoanhSoMoi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBaoCaoDoanhSoMoi.Name = "btnBaoCaoDoanhSoMoi";
-            this.btnBaoCaoDoanhSoMoi.Size = new System.Drawing.Size(122, 28);
+            this.btnBaoCaoDoanhSoMoi.Size = new System.Drawing.Size(163, 34);
             this.btnBaoCaoDoanhSoMoi.TabIndex = 16;
             this.btnBaoCaoDoanhSoMoi.Text = "Báo cáo mới";
             this.btnBaoCaoDoanhSoMoi.UseVisualStyleBackColor = true;
@@ -78,21 +80,23 @@
             // 
             this.btnLapBaoCaoDoanhSo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnLapBaoCaoDoanhSo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLapBaoCaoDoanhSo.Location = new System.Drawing.Point(775, 102);
-            this.btnLapBaoCaoDoanhSo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLapBaoCaoDoanhSo.Location = new System.Drawing.Point(1033, 126);
+            this.btnLapBaoCaoDoanhSo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLapBaoCaoDoanhSo.Name = "btnLapBaoCaoDoanhSo";
-            this.btnLapBaoCaoDoanhSo.Size = new System.Drawing.Size(122, 28);
+            this.btnLapBaoCaoDoanhSo.Size = new System.Drawing.Size(163, 34);
             this.btnLapBaoCaoDoanhSo.TabIndex = 14;
             this.btnLapBaoCaoDoanhSo.Text = "Lập báo cáo";
             this.btnLapBaoCaoDoanhSo.UseVisualStyleBackColor = true;
+            this.btnLapBaoCaoDoanhSo.Click += new System.EventHandler(this.btnLapBaoCaoDoanhSo_Click);
             // 
             // textBoxTongDoanhThu
             // 
             this.textBoxTongDoanhThu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxTongDoanhThu.Location = new System.Drawing.Point(339, 175);
+            this.textBoxTongDoanhThu.Location = new System.Drawing.Point(452, 215);
+            this.textBoxTongDoanhThu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxTongDoanhThu.Name = "textBoxTongDoanhThu";
             this.textBoxTongDoanhThu.ReadOnly = true;
-            this.textBoxTongDoanhThu.Size = new System.Drawing.Size(185, 26);
+            this.textBoxTongDoanhThu.Size = new System.Drawing.Size(245, 30);
             this.textBoxTongDoanhThu.TabIndex = 13;
             // 
             // dataGridViewBaoCaoDoanhSo
@@ -110,43 +114,48 @@
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dataGridViewBaoCaoDoanhSo.Location = new System.Drawing.Point(110, 210);
-            this.dataGridViewBaoCaoDoanhSo.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewBaoCaoDoanhSo.Location = new System.Drawing.Point(147, 258);
+            this.dataGridViewBaoCaoDoanhSo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridViewBaoCaoDoanhSo.Name = "dataGridViewBaoCaoDoanhSo";
             this.dataGridViewBaoCaoDoanhSo.ReadOnly = true;
             this.dataGridViewBaoCaoDoanhSo.RowHeadersWidth = 51;
             this.dataGridViewBaoCaoDoanhSo.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.dataGridViewBaoCaoDoanhSo.RowTemplate.Height = 24;
-            this.dataGridViewBaoCaoDoanhSo.Size = new System.Drawing.Size(940, 254);
+            this.dataGridViewBaoCaoDoanhSo.Size = new System.Drawing.Size(1253, 313);
             this.dataGridViewBaoCaoDoanhSo.TabIndex = 12;
             // 
             // Column1
             // 
             this.Column1.HeaderText = "STT";
+            this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
             this.Column1.ReadOnly = true;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Hiệu xe";
+            this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
             this.Column2.ReadOnly = true;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "Số lượt sửa";
+            this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
             this.Column3.ReadOnly = true;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "Thành tiền";
+            this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
             this.Column4.ReadOnly = true;
             // 
             // Column5
             // 
             this.Column5.HeaderText = "Tỷ lệ";
+            this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
             this.Column5.ReadOnly = true;
             // 
@@ -154,10 +163,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(202, 182);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(269, 224);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 19);
+            this.label1.Size = new System.Drawing.Size(141, 22);
             this.label1.TabIndex = 21;
             this.label1.Text = "Tổng doanh thu :";
             // 
@@ -166,15 +174,16 @@
             this.lblCapNhatQuyDinh.BackColor = System.Drawing.Color.Transparent;
             this.lblCapNhatQuyDinh.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.lblCapNhatQuyDinh.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblCapNhatQuyDinh.Location = new System.Drawing.Point(497, 67);
+            this.lblCapNhatQuyDinh.Location = new System.Drawing.Point(663, 82);
+            this.lblCapNhatQuyDinh.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCapNhatQuyDinh.Name = "lblCapNhatQuyDinh";
-            this.lblCapNhatQuyDinh.Size = new System.Drawing.Size(218, 27);
+            this.lblCapNhatQuyDinh.Size = new System.Drawing.Size(291, 33);
             this.lblCapNhatQuyDinh.TabIndex = 22;
             this.lblCapNhatQuyDinh.Text = "DOANH SỐ";
             // 
             // Baocaodoanhso
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.Controls.Add(this.lblCapNhatQuyDinh);
@@ -185,8 +194,9 @@
             this.Controls.Add(this.btnLapBaoCaoDoanhSo);
             this.Controls.Add(this.textBoxTongDoanhThu);
             this.Controls.Add(this.dataGridViewBaoCaoDoanhSo);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Baocaodoanhso";
-            this.Size = new System.Drawing.Size(1089, 668);
+            this.Size = new System.Drawing.Size(1452, 822);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBaoCaoDoanhSo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Source code/GarageManager/GarageManager/usercontrol/Baocaodoanhso.cs
+++ Source code/GarageManager/GarageManager/usercontrol/Baocaodoanhso.cs
@@ -12,9 +12,46 @@ namespace GarageManager.usercontrol
 {
     public partial class Baocaodoanhso : UserControl
     {
+        int month;
+        int year;
         public Baocaodoanhso()
         {
             InitializeComponent();
+            month = DateTime.Now.Month;
+            year = DateTime.Now.Year;
+            textBoxThangBaoCao.Text = month.ToString() + '/' + year.ToString();
+            LoadData();
+        }
+        
+        void LoadData()
+        {
+            Model.BAOCAODOANHSO financialReport = Classes.Finance.GetMonthlyFinancialReport(month, year);
+            int stt = 1;
+            foreach (var item in financialReport.CT_BCDS)
+            {
+                Model.HIEUXE xe = Classes.DataProvider.Instance.DB.HIEUXEs.FirstOrDefault(x => x.MaHieuXe == item.MaHieuXe);
+                string brand = xe.TenHieuXe.ToString();
+                dataGridViewBaoCaoDoanhSo.Rows.Add(stt.ToString(), brand, item.SoLuotSua.ToString(), item.ThanhTien.ToString());
+                stt++;
+            }
+        }
+
+        private void btnLapBaoCaoDoanhSo_Click(object sender, EventArgs e)
+        {
+            string[] txt = textBoxThangBaoCao.Text.Split('/');
+            month = Convert.ToInt32(txt[0]);
+            year = Convert.ToInt32(txt[1]);
+            if (year > DateTime.Now.Year)
+            {
+                MessageBox.Show("Thời gian lớn hơn thời gian hiện tại", "Lập báo cáo không thành công", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (month > DateTime.Now.Month)
+            {
+                MessageBox.Show("Thời gian lớn hơn thời gian hiện tại", "Lập báo cáo không thành công", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            LoadData();
         }
     }
 }
